f9c175d6af32ab41a24a34ad4334dc2492f0a5f0
Lucas Cunha <lucas_cunha_souza@hotmail.com>
2023-04-06 08:33:46 -0300

    Inserindo novamente busca

--- Binary-Tree-Lucas/src/BynaryTree/BynaryTree.java
+++ Binary-Tree-Lucas/src/BynaryTree/BynaryTree.java
@@ -1,3 +1,76 @@
 package BynaryTree;
-public class BynaryTree {
+public class BynaryTree<T extends Comparable <T>>{
+    private Node<T> prim;
+    private int folhas;
+
+    public BynaryTree(){
+        this.prim = null;
+        this.folhas = 0;
+    }
+
+    public Node getPrim(){
+        return this.prim;
+    }
+
+    public void insert(T elem){
+        Node<T> novo = new Node<T>(elem);
+        Node<T> noatual = this.getPrim();
+        boolean parar = false;
+
+        if(noatual == null){
+            this.prim = novo;
+        }
+        else{
+            while(!parar){
+                if(novo.getValor().compareTo(noatual.getValor()) < 0){
+                    //noatual = noatual.getEsq();
+                    if(noatual.getEsq() == null){
+                        noatual.setEsq(novo);
+                        parar = true;
+                    }else{
+                        noatual = noatual.getEsq();
+                    }
+                } else if (novo.getValor().compareTo(noatual.getValor()) > 0) {
+                    //noatual = noatual.getEsq();
+                    if(noatual.getDir() == null){
+                        noatual.setDir(novo);
+                        parar = true;
+                    }else{
+                        noatual = noatual.getDir();
+                    }
+                }
+                else{
+                    System.out.printf("Valor %s ja existe na arvore.", noatual.getValor().toString()
+                    );
+                }
+            }
+        }
+    }
+
+    public boolean search(T elem){
+        boolean foundit = false;
+        boolean parar = false;
+        Node<T> searchto = new Node<T>(elem);
+        Node<T> noatual = this.getPrim();
+
+        if(noatual != null){
+            while(!parar){
+                if(searchto.getValor().compareTo(noatual.getValor()) < 0){
+                    noatual = noatual.getEsq();
+                    if(noatual.getValor() == null){
+                        parar = true;
+                    }
+                }                    }
+                } else if (searchto.getValor().compareTo(noatual.getValor()) > 0) {
+                    noatual = noatual.getDir();
+                    if(noatual.getValor() == null){
+                        parar = true;
+                    }
+                }
+                else{
+                    foundit = true;
+                    parar = true;
+                }
+        return foundit;
+    }
 }
--- Binary-Tree-Lucas/src/BynaryTree/Node.java
+++ Binary-Tree-Lucas/src/BynaryTree/Node.java
@@ -1,11 +1,27 @@
 package BynaryTree;
 
-public class Node<T> {
+public class Node<T extends Comparable> {
     private T valor;
-    private Node prox;
+    private Node esq, dir;
 
     public Node(T valor){
         this.valor = valor;
-        this.prox = null;
+        this.esq = null;
+        this.dir = null;
+    }
+    public Node getEsq(){
+        return this.esq;
+    }
+    public void setEsq(Node<T> no){
+        this.esq = no;
+    }
+    public Node getDir(){
+        return this.dir;
+    }
+    public void setDir(Node<T> no){
+        this.dir = no;
+    }
+    public T getValor(){
+        return this.valor;
     }
 }
